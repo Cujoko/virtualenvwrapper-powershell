60346c34f42583ab6959efe9cac95a770f81f802
Cujoko <cujoko@gmail.com>
2023-02-03 22:28:56 +0900

    Add autocompletion

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,19 @@
 $PowerShellProfile = $PROFILE.CurrentUserAllHosts
 $PowerShellPath = Split-Path $PowerShellProfile
 Import-Module $PowerShellPath\Modules\VirtualEnvWrapper.psm1
+
+Register-ArgumentCompleter -Native -CommandName workon -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+    $WORKON_HOME=$Env:WORKON_HOME
+
+    if (!$WORKON_HOME) {
+        $WORKON_HOME = "$Env:USERPROFILE\Envs"
+    }
+
+    (Get-ChildItem -Path $WORKON_HOME).Name | Where-Object {
+        $_ -like "$wordToComplete*"
+    } | ForEach-Object {
+          "$_"
+    }
+}
